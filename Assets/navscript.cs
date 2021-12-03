938bd7a7cda2110b39b5b761dd6ae15681003997
ryan <514875@chrome.escg.ac.uk>
2021-12-03 09:57:01 +0000

    first commit

---
+++ Assets/navscript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navscript: MonoBehaviour
+{
+    public Transform goal;
+    public NavMeshAgent agent;
+
+    void Start() { agent.destination = goal.position; }
+}
