8ba0b9a6122820bf5adb3b67cbfc513271788c87
ryan <514875@chrome.escg.ac.uk>
2021-12-03 13:19:42 +0000

    second commmit

---
+++ Assets/node.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class node : MonoBehaviour
+{
+
+
+    public bool stop = false;
+
+    public nodeManager nm;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            nm.id++;
+            nm.transformer = nm.nodes[nm.id].transform;
+            if (stop)
+            {
+                
+            }
+            else { }
+        }
+    }
+
+
+}
---
+++ Assets/nodeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nodeManager : MonoBehaviour
+{
+    public List<GameObject> nodes;
+
+
+    public int id = 0;
+
+    public Transform transformer;
+    public void Start() {
+        transformer = nodes[id].transform;
+    }
+    public GameObject Player;
+
+
+    public void Update()
+    {
+        Vector3 direction = Player.transform.position - transformer.position;
+        Player.transform.position += Vector3.Normalize(direction);
+    }
+}
